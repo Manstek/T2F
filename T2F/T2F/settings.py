a053d52a7fb2f134a25464c846ae29e1703a0e70
Aleksandr Morozov <morozov460336@yandex.ru>
2024-09-03 11:04:54 +0300

    connect PostgresSQL

--- T2F/T2F/settings.py
+++ T2F/T2F/settings.py
@@ -57,10 +57,15 @@ WSGI_APPLICATION = 'T2F.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'T2F',
+        'USER': 'postgres',
+        'PASSWORD': 'r322ff',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
